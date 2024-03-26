05c481d9d3a93038fd299fd3efd4084b1cd69800
Pulkit Sharma <pulkit.sharma@students.iiit.ac.in>
2024-03-26 19:28:22 +0530

    debugging output for search query

--- service.py
+++ service.py
@@ -37,21 +37,21 @@ class Service:
 
     def get_videos(self,page,per_page):
 
-        insert_data_in_database = Database_Handler()
+        database_handler = Database_Handler()
         # Fetch video data
-        video_data = insert_data_in_database.fetch_videos(page, per_page)
+        video_data = database_handler.fetch_videos(page, per_page)
 
         # Format response
         response = []
         for video in video_data:
             response.append({
-                'title': video[1],
-                'description': video[2],
-                'publishing_datetime': str(video[3]),
+                'title': video[2],
+                'description': video[3],
+                'publishing_datetime': str(video[4]),
                 'thumbnails': {
-                    'default': video[4],
-                    'medium': video[5],
-                    'high': video[6]
+                    'default': video[5],
+                    'medium': video[6],
+                    'high': video[7]
                 }
             })
 
@@ -59,21 +59,21 @@ class Service:
     
     def search_videos(self,title_query,description_query):
 
-        insert_data_in_database = Database_Handler()
+        database_handler = Database_Handler()
         # Perform search
-        search_results = insert_data_in_database.search_videos(title_query, description_query)
+        search_results = database_handler.search_videos(title_query, description_query)
 
         # Format response
         response = []
         for video in search_results:
             response.append({
-                'title': video[1],
-                'description': video[2],
-                'publishing_datetime': str(video[3]),
+                'title': video[2],
+                'description': video[3],
+                'publishing_datetime': str(video[4]),
                 'thumbnails': {
-                    'default': video[4],
-                    'medium': video[5],
-                    'high': video[6]
+                    'default': video[5],
+                    'medium': video[6],
+                    'high': video[7]
                 }
             })
         return response
